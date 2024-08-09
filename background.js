f0e71c245e1cef77160176ad72d5ee1493dd225d
Carlos Barros <55989012+barros-carlos@users.noreply.github.com>
2024-08-09 20:15:34 -0300

    fix: some ajusts

--- background.js
+++ background.js
@@ -2,3 +2,22 @@ console.log('background.js running');
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+
+// if needed realtime info from tabs on change
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        chrome.scripting.insertCSS({
+            target: { tabId: tabId },
+            files: ['foreground.css']
+        }).then(() => {
+            console.log('foreground.css injected');
+        }).catch(err => console.log(err));
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ['foreground.js']
+        }).then(() => {
+            console.log('foreground.js injected');
+        }).catch(err => console.log(err));
+    }
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,5 @@
-function minhapikaflamejante(){
-    console.log('minhapikaflamejante');
-    const pika = document.getElementById('empresaNome');
-    pika.innerHTML = 'Minha Pika Flamejante';
-}
-
-aletr('popup.js');
+console.log('popup.js');
 
 function mudar(){
-    window.location.href = 'https://www.google.com.br';
+
 }
