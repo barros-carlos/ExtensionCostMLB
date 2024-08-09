55472ceb5a99102f0ba424351ce55b5f27bf8b46
Carlos Barros <55989012+barros-carlos@users.noreply.github.com>
2024-08-09 15:55:17 -0300

    fix: miafest correctly done

---
+++ background.js
@@ -0,0 +1,4 @@
+console.log('background.js running');
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
--- popup.js
+++ popup.js
@@ -1,22 +1,11 @@
-function processarValores(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
+function minhapikaflamejante(){
+    console.log('minhapikaflamejante');
+    const pika = document.getElementById('empresaNome');
+    pika.innerHTML = 'Minha Pika Flamejante';
+}
 
-    // Obtém os valores dos campos de entrada
-    const valorTextoNumerico = document.getElementById('valorTextoNumerico').value;
-    const valorPorcentagem = parseFloat(document.getElementById('valorPorcentagem').value);
-    const valorReais = parseFloat(document.getElementById('valorReais').value);
+aletr('popup.js');
 
-    // Formata o valor em reais como moeda
-    const valorReaisFormatado = valorReais.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    });
-
-    // Exibe os valores no console
-    console.log('Texto Numérico:', valorTextoNumerico);
-    console.log('Porcentagem:', valorPorcentagem + '%');
-    console.log('Valor em Reais:', valorReaisFormatado);
-
-    // Exibir uma mensagem para o usuário
-    alert(`Texto Numérico: ${valorTextoNumerico}\nPorcentagem: ${valorPorcentagem}%\nValor em Reais: ${valorReaisFormatado}`);
+function mudar(){
+    window.location.href = 'https://www.google.com.br';
 }
